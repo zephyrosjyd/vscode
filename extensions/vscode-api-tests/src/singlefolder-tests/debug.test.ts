559c1329fa2127b70e484787b8c9ffdf4d9ec9e0
isidor <inikolic@microsoft.com>
2020-02-12 15:11:06 +0100

    trigger 8

--- extensions/vscode-api-tests/src/singlefolder-tests/debug.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/debug.test.ts
@@ -38,7 +38,7 @@ suite('Debug', function () {
 	});
 
 	test('start debugging', async function () {
-		console.log('        ');
+		console.log('         ');
 		assert.equal(debug.activeDebugSession, undefined);
 		let stoppedEvents = 0;
 		let variablesReceived: () => void;
